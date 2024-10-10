f65c871d05dce1f96eabc2d9e076ebf96dcbdf2d
Andrey Markin <amarkin673@gmail.com>
2024-10-10 14:57:49 +0300

    mobx App

---
+++ src/store/users.js
@@ -0,0 +1,23 @@
+import {types, flow} from 'mobx-state-tree';
+import apiCall from '../api'
+
+const User = types.model('User', {
+    id: types.identifier,
+    createdAt: types.string,
+    name: types.string,
+    avatar: types.string,
+})
+
+const UsersStore = types.model('UsersStore', {
+    users: types.maybe(types.array(User))
+}).actions(self =>{
+    return {
+        load: flow(function* () {
+            self.users = yield apiCall.get('users');
+        })
+    }
+})
+
+
+export default UsersStore;
+
